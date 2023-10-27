edf4471cbdeade3799171a39fa85b704710842f0
Armaan-Singh-Dhillon <dhillonarmaan2004@gmail.com.com>
2023-10-27 13:28:48 +0530

    bare setup done

--- App.js
+++ App.js
@@ -1,14 +1,95 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { ScrollView, StyleSheet, Text, TextInput, View } from 'react-native';
+import { useState } from 'react';
 export default function App() {
+  const [data, setData] = useState([
+
+    {
+      "A": 'A',
+      "B": 'B',
+      "C": 'C',
+      "D": 'D',
+      "E": 'E',
+    },
+    {
+      "A": 1,
+      "B": 1,
+      "C": 1,
+      "D": 1,
+      "E": 1,
+    },
+    {
+      "A": 1,
+      "B": 1,
+      "C": 1,
+      "D": 1,
+      "E": 1,
+    },
+    {
+      "A": 1,
+      "B": 1,
+      "C": 1,
+      "D": 1,
+      "E": 1,
+    },
+    {
+      "A": 1,
+      "B": 1,
+      "C": 1,
+      "D": 1,
+      "E": 1,
+    },
+
+  ]);
+
+  const handleChange = (newValue, rowIndex, key) => {
+    const updatedData = [...data];
+    updatedData[rowIndex][key] = newValue;
+    setData(updatedData);
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ScrollView>
+      <View style={styles.container}>
+
+        {data.map((el, rowIndex) => (
+
+          <View key={rowIndex} style={{ flexDirection: 'row' }}>
+            <View style={{
+              width: 50,
+              borderColor: 'grey',
+              borderWidth: 2,
+              color: 'black',
+              alignItems: 'center'
+            }}>
+              <Text>
+                {rowIndex}
+              </Text>
+            </View>
+            {Object.entries(el).map(([key, val], index) => (
+
+
+              <TextInput
+                key={key}
+                style={{
+                  width: 50,
+                  borderColor: 'grey',
+                  borderWidth: 2,
+                  color: 'black',
+                  textAlign: "center"
+                }}
+                value={val + ''}
+                onChangeText={(text) => handleChange(text, rowIndex, key)}
+              />
+            ))}
+          </View>
+        ))}
+        <TextInput style={{ width: 100 }} />
+      </View>
+    </ScrollView>
   );
 }
+
 
 const styles = StyleSheet.create({
   container: {
