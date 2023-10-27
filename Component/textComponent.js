c900bd6767f33189714585e55b1edd0a862f4af8
Armaan-Singh-Dhillon <dhillonarmaan2004@gmail.com.com>
2023-10-27 22:07:33 +0530

    Text Component Separated for additional state

---
+++ Component/textComponent.js
@@ -0,0 +1,35 @@
+import { View, Text, TextInput, StyleSheet } from 'react-native'
+import React from 'react'
+import { useState } from 'react'
+import MyContext from '../Context/MyContext'
+import { useContext } from 'react'
+const TextComponent = ({ rowIndex, col }) => {
+    const { data } = useContext(MyContext)
+    console.log(rowIndex, col)
+    const handleChange = () => {
+        data[rowIndex][col] = input
+    };
+
+    const [input, setInput] = useState("" || data[rowIndex][col])
+    return (
+        <View>
+            <TextInput
+                style={cellStyle}
+                value={input}
+                onChangeText={(data) => setInput(data)}
+                onEndEditing={handleChange}
+            />
+        </View>
+    )
+}
+const cellStyle = StyleSheet.create({
+    width: 120,
+    borderColor: 'grey',
+    borderWidth: 1,
+    color: 'black',
+    textAlign: "center",
+    padding: 2.5
+
+})
+
+export default TextComponent
